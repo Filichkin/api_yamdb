f07943a1627e39e428c468d84532ba865bf3f46a
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-03-31 21:14:57 +0300

    updated requirements.txt

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -6,7 +6,7 @@ from django.core.exceptions import ValidationError
 def validate_username(username):
     if username.lower() in ('me',):
         raise ValidationError(
-            f'Зарезервированный логин {username}, нельзя использлвать'
+            f'Зарезервированный логин {username}, нельзя использолвать'
         )
     if not re.match(r'^[\w.@+-]+\Z', username):
         raise ValidationError(
