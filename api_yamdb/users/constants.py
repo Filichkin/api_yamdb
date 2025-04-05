87e4602dcd14c0571f2dbff78ab52556ce6751f7
Alexey Filichkin <filichkin.alexey@gmail.com>
2025-04-05 14:12:10 +0300

    blanks deleted in users constants

--- api_yamdb/users/constants.py
+++ api_yamdb/users/constants.py
@@ -1,7 +1,7 @@
 ROLE_CHOICES = (
-               ('user', 'Пользователь'),
-               ('moderator', 'Модератор'),
-               ('admin', 'Администратор'),
+    ('user', 'Пользователь'),
+    ('moderator', 'Модератор'),
+    ('admin', 'Администратор'),
 )
 MAX_LENGTH_NAME = 150
 MAX_LENGTH_EMAIL = 254
